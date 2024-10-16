98e39e7a549bae2d0fe3d85f2cf08d65c61545f4
SureshKaji <sureshkajeepan@gmail.com>
2024-10-16 12:58:29 +0530

    Aceternity UI

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,15 @@
+
+import LampDemo from '@/components/ui/lamp'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      
+      <LampDemo/>
+
+    </div>
+  )
+}
+
+export default Hero
